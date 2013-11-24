148398a25885a6ecbed8ab1cc91014638811d10f
Storm <storm@storm.(none)>
2013-11-24 20:57:23 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fit::Application.config.secret_token = '6307242bd2d67ae1f366cf9d82f97100472915d2e422ee80f4a07390469b9de5910173589d2a93636ac7ba9fc34e56b91365f3de5e515009bbc962688e8bd1f0'
