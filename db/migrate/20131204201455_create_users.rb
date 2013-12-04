6e9fda05f451d333b794323f61913909798ef565
Raplider <Raplider@gmail.com>
2013-12-04 22:23:36 +0200

    add medel User

---
+++ db/migrate/20131204201455_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :midlename
+      t.string :lasname
+      t.string :position
+      t.string :department
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
