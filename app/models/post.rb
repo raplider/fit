848a3c74bb55e6505cc90287c9c8a3210d02854d
VasiliyMaximov <sav2icon@gmail.com>
2014-01-08 19:32:37 +0200

    labels add

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Post < ActiveRecord::Base
   belongs_to :admin
+  attr_accessible :title, :body, :file, :category, :form_of_study
   has_attached_file :file
   validates_attachment :file,
     :content_type => {:content_type => ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/pdf", "text/plain"] }
+  validates :title, :body, :presence => true
+
   default_scope :order => 'posts.created_at DESC'
 
   self.per_page = 4
